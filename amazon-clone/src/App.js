c6b5ce0bc86a5c7eba35fbfc6fdc6721e75b2bb4
Krish Nachnani <krishnachnani@icloud.com>
2025-07-15 17:46:34 -0500

    Update 1

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,15 @@
 import REACT from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+
+      <Header />
+      {/* Home  */}
+      {/*  */}
+      
     </div>
   );
 }
---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className='header__logo' src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo.png"/>
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" /> 
+        {/* Logo  */}
+      </div>
+
+      <div className="header__nav">
+
+        <div className="header__option">
+            <span className="header__optionLineOne">Hello User</span>
+            <span className="header__optionLineOne">Sign in</span>
+        </div>
+
+        <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineOne">& Orders</span>
+        </div>
+
+        <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineOne">Prime</span>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
