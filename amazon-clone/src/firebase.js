b83d587bc2b2a2c1a5c1619416297b2590266bf3
Krish Nachnani <krishnachnani@icloud.com>
2025-07-15 17:20:12 -0500

    Blank canvas

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAGdUlMoMzfiXRpcpLjelYdCPOn837akQ",
+  authDomain: "clone-e8b79.firebaseapp.com",
+  projectId: "clone-e8b79",
+  storageBucket: "clone-e8b79.firebasestorage.app",
+  messagingSenderId: "482942629980",
+  appId: "1:482942629980:web:bd2db82048d7c99a4fce99",
+  measurementId: "G-Q0SVJWKZGZ"
+};
+ 
