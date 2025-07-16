ed98e76d46c254f47bb4290631947e14cb4c33b4
Krish Nachnani <krishnachnani@icloud.com>
2025-07-15 21:37:50 -0500

    header almost done

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
     <div className='header'>
-      <img className='header__logo' src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo.png"/>
+      <img className='header__logo' src="https://www.pngmart.com/files/23/Amazon-Logo-White-PNG-Image.png"/>
 
       <div className="header__search">
-        <input className="header__searchInput" type="text" /> 
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon"></SearchIcon> 
       </div>
 
       <div className="header__nav">
 
         <div className="header__option">
             <span className="header__optionLineOne">Hello User</span>
-            <span className="header__optionLineOne">Sign in</span>
+            <span className="header__optionLineTwo">Sign in</span>
         </div>
 
         <div className="header__option">
             <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineOne">& Orders</span>
+            <span className="header__optionLineTwo">& Orders</span>
         </div>
 
         <div className="header__option">
             <span className="header__optionLineOne">Your</span>
-            <span className="header__optionLineOne">Prime</span>
+            <span className="header__optionLineTwo">Prime</span>
         </div>
 
       </div>
